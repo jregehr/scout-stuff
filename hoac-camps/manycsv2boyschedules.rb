f0b2e438c832cb153b49a1b19cec0cda75603155
jregehr <10343188+jregehr@users.noreply.github.com>
2021-07-14 01:58:29 -0500

    Converted to multi-colunmn.

--- hoac-camps/manycsv2boyschedules.rb
+++ hoac-camps/manycsv2boyschedules.rb
@@ -19,14 +19,14 @@ def parse_time_slot(time_slots)
   return "" if de_nil(time_slots).length() < 2
 
   # Same badge all six days 
-  return "\"#{time_slots[1]}\"" if (!time_slots[1].nil? && time_slots[1].start_with?('Trail To First Class')) || (!has_nils(time_slots[1,6]) && de_nil(time_slots[1,6]).uniq.length() == 1)
+  return "#{time_slots[1]}" if (!time_slots[1].nil? && time_slots[1].start_with?('Trail To First Class')) || (!has_nils(time_slots[1,6]) && de_nil(time_slots[1,6]).uniq.length() == 1)
 
   if count_sames(time_slots) > 3
     return do_sames(time_slots)
 
   end
 
-  slot = "\""
+  slot = ""
 
   if !has_nils(time_slots[1,3]) && de_nil(time_slots[1,3]).uniq.length() == 1
     slot += "#{time_slots[1]}-2"
@@ -43,7 +43,6 @@ def parse_time_slot(time_slots)
 
   slot += " / #{slot2}" if !slot2.empty? 
 
-  slot += "\""
   return slot
 
 end
@@ -65,14 +64,12 @@ def count_sames(slots)
 end
 
 def do_sames(slots)
-  slot = "\""
 
   slot += slots[1]
   1.upto(5) do |i|
     slot += ", #{slots[i]}-#{i}" if slots[i] != slots[1]
   end
 
-  slot += "\""
   return slot
 end
 
@@ -97,29 +94,41 @@ def find_time_slot(time_slot, input_table)
 
 end
 
-def parse_intput_file(input_file, output_file)
+def parse_input_file(input_file, output_array, row, col)
   input_table = CSV.read(input_file)
+
+  STDOUT.puts("DEBUG: #{input_file}")
+  # STDOUT.puts("DEBUG: #{output_array}")
+  STDOUT.puts("DEBUG: #{row}, #{col}")
 
   if input_table[1].join(",") != ',Day 2 - 07/17/2021,Day 4 - 07/19/2021,Day 5 - 07/20/2021,Day 6 - 07/21/2021,Day 7 - 07/22/2021,Day 8 - 07/23/2021'
     STDOUT.puts "Error in #{input_file}: Unknown schedule days: #{input_table[1].join(",")}"
     return
   end
-  
+
   name_blag = input_table[0][0].split(' ')
   # kid_name = "\"#{name_blag[0]} #{name_blag[1][0,1]}\""
-  kid_name = "\"#{name_blag[0]} #{name_blag[1]}\""
-  output_file.puts(kid_name)
+  kid_name = "#{name_blag[0]} #{name_blag[1]}"
+  output_array[row][col] = kid_name
+  row += 1
   
   time_slots = ["08:00 AM", "08:30 AM", "09:30 AM", "10:00 AM", "02:00 PM", "03:00 PM", "03:30 PM"]
 
   time_slots.each do |slot|
     row_badges = ""
     slot_row = find_time_slot(slot, input_table[2,6])
+
     if !slot_row.nil?
       row_badges = parse_time_slot(slot_row)
     end
-      output_file.puts("\"#{slot[0,5].sub!(/^0?/, "")}\",#{row_badges}")
+
+    output_array[row][col] = "#{slot[0,5].sub!(/^0?/, "")}"
+    output_array[row][col+1] = "#{row_badges}"
+    row += 1
+
   end
+
+  return row
 
 end
 
@@ -128,17 +137,35 @@ check_params
 input_folder = ENV["CAMPER_SCHEDULES"]
 output_folder = ENV["OUTPUT_FOLDER"]
 current_date = DateTime.now.strftime "%Y-%m-%d_%H-%M-%S"
+arow = 0
+acol = 0
+first_col = true
 
-open("#{output_folder}/schedules_#{current_date}.csv", 'w+') do |output_file|
+# open("#{output_folder}/schedules_#{current_date}.csv", 'w+') do |output_file|
 
-  STDOUT.puts "To process: #{Dir["#{input_folder}/*.csv"].length()}"
+length = Dir["#{input_folder}/*.csv"].length()
+STDOUT.puts "To process: #{length}"
 
-  Dir["#{input_folder}/*.csv"].each do |input_file|
+output_array = Array.new(((length+2)*8)/2){Array.new(4)}
+# output_array = Array.new(250){Array.new(4)}
 
-    STDOUT.puts "File: #{File.basename(input_file)}"
-    parse_intput_file input_file, output_file
-    
-  
-  end     # Dir[input_folder].each do |input_file|
+count = 0
 
-end       # open("outputs/schedules_#{current_date}.csv", 'w+') do |output_file|
+Dir["#{input_folder}/*.csv"].each do |input_file|
+
+  STDOUT.puts "File: #{File.basename(input_file)}"
+  arow = parse_input_file(input_file, output_array, arow, acol)
+  count += 1
+  if first_col && count > length/2
+    arow = 0
+    acol = 2
+    first_col = false
+  end
+
+end     # Dir[input_folder].each do |input_file|
+
+CSV.open("#{output_folder}/schedules_#{current_date}.csv", 'wb') do |output_file|
+  output_array.each do |array_row|
+    output_file << array_row
+  end
+end
